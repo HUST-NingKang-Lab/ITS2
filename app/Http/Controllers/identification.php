ff0b59121f465803d698231194cc846658861317
Shaojun Yu <1024527135@qq.com>
2017-12-20 10:52:47 +0800

    no message

--- app/Http/Controllers/identification.php
+++ app/Http/Controllers/identification.php
@@ -30,35 +30,55 @@ class identification extends Controller
 
     public function getResult($task_hash)
     {
-        if (Redis::hget($task_hash,'identification_result')) {
-            $res = [
-                   'identification_result' => Redis::hget($task_hash,'identification_result'),
-                   'genus_abd' => Redis::hget($task_hash,'genus_abd'),
-                   'genus_pie' => Redis::hget($task_hash,'genus_pie'),
-                   'species_abd' => Redis::hget($task_hash,'species_abd'),
-                   'species_pie' => Redis::hget($task_hash,'species_pie'),
-               ];
-            return JsonResponse::create($res);
-        }
-//        return Redis::hget($task_hash,'identification_result');
         $path = "/home/its/web_processing/" . $task_hash . "/";
-        $command = "[ -d /home/its/web_processing/" . $task_hash . " ] && echo true || echo false";
-        SSH::run($command, function ($line) use ($path, $task_hash) {
-            $res = $line . PHP_EOL;
-            if (strpos($res, 'true') == 0) {
-                $identification_result = SSH::into('production')->getString($path . "identification_result.txt",'identification_result.txt');
-                $genus_abd = SSH::into('production')->getString($path . "genus_abundance_table.txt",'genus_abundance_table.txt');
-                $genus_pie = SSH::into('production')->getString($path . "genus_abd_top_table.txt",'genus_abd_top_table.txt');
-                $species_abd = SSH::into('production')->getString($path . "species_abundance_table.txt",'species_abundance_table.txt');
-                $species_pie = SSH::into('production')->getString($path . "species_abd_top_table.txt",'species_abd_top_table.txt');
-                Redis::hset($task_hash,'identification_result',$identification_result);
-                Redis::hset($task_hash,'genus_abd',trim($genus_abd));
-                Redis::hset($task_hash,'genus_pie',trim($genus_pie));
-                Redis::hset($task_hash,'species_abd',trim($species_abd));
-                Redis::hset($task_hash,'species_pie',trim($species_abd));
-//                var_dump($identification_result);
-            }
-        });
+
+        if (file_exists($path .  "identification_result.txt")){
+            $identification_result = file_get_contents($path . "identification_result.txt");
+            $genus_abd = file_get_contents($path . "genus_abundance_table.txt");
+            $genus_pie = file_get_contents($path . "genus_abd_top_table.txt");
+            $species_abd = file_get_contents($path . "species_abundance_table.txt");
+            $species_pie = file_get_contents($path . "species_abd_top_table.txt");
+            $res = [
+                'identification_result' => $identification_result,
+                'genus_abd' => $genus_abd,
+                'genus_pie' => $genus_pie,
+                'species_abd' => $species_abd,
+                'species_pie' => $species_pie,
+            ];
+            return JsonResponse::create($res);
+
+        }else{
+            return '';
+        }
+//        if (Redis::hget($task_hash,'identification_result')) {
+//            $res = [
+//                   'identification_result' => Redis::hget($task_hash,'identification_result'),
+//                   'genus_abd' => Redis::hget($task_hash,'genus_abd'),
+//                   'genus_pie' => Redis::hget($task_hash,'genus_pie'),
+//                   'species_abd' => Redis::hget($task_hash,'species_abd'),
+//                   'species_pie' => Redis::hget($task_hash,'species_pie'),
+//               ];
+//            return JsonResponse::create($res);
+//        }
+////        return Redis::hget($task_hash,'identification_result');
+//        $path = "/home/its/web_processing/" . $task_hash . "/";
+//        $command = "[ -d /home/its/web_processing/" . $task_hash . " ] && echo true || echo false";
+//        SSH::run($command, function ($line) use ($path, $task_hash) {
+//            $res = $line . PHP_EOL;
+//            if (strpos($res, 'true') == 0) {
+//                $identification_result = SSH::into('production')->getString($path . "identification_result.txt",'identification_result.txt');
+//                $genus_abd = SSH::into('production')->getString($path . "genus_abundance_table.txt",'genus_abundance_table.txt');
+//                $genus_pie = SSH::into('production')->getString($path . "genus_abd_top_table.txt",'genus_abd_top_table.txt');
+//                $species_abd = SSH::into('production')->getString($path . "species_abundance_table.txt",'species_abundance_table.txt');
+//                $species_pie = SSH::into('production')->getString($path . "species_abd_top_table.txt",'species_abd_top_table.txt');
+//                Redis::hset($task_hash,'identification_result',$identification_result);
+//                Redis::hset($task_hash,'genus_abd',trim($genus_abd));
+//                Redis::hset($task_hash,'genus_pie',trim($genus_pie));
+//                Redis::hset($task_hash,'species_abd',trim($species_abd));
+//                Redis::hset($task_hash,'species_pie',trim($species_abd));
+////                var_dump($identification_result);
+//            }
+//        });
 //        return JsonResponse::create([]);
     }
 }
